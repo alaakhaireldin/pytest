4fa4da4e56b75f9ad103ceff08363c57041e5c18
root <root@DESKTOP-OLPLAOG.localdomain>
2022-06-24 11:47:39 +0200

    removing most tests

--- tests/00_empty_test.py
+++ tests/00_empty_test.py
@@ -1,17 +1,39 @@
-def test_empty():
-    """
-    PyTest tests are callables whose names start with "test"
-    (by default)
-
-    It looks for them in modules whose name starts with "test_" or ends with "_test"
-    (by default)
-    """
-    pass
 
 
-def empty_test():
-    """
-    My name doesn't start with "test", so I won't get run.
-    (by default ;-)
-    """
-    pass
+from other_code.utility import add_numbers
+
+
+def test_things():
+    assert add_numbers([1, 4, 6]) == 11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def test_empty():
+#     """
+#     PyTest tests are callables whose names start with "test"
+#     (by default)
+
+#     It looks for them in modules whose name starts with "test_" or ends with "_test"
+#     (by default)
+#     """
+#     pass
+
+
+# def empty_test():
+#     """
+#     My name doesn't start with "test", so I won't get run.
+#     (by default ;-)
+#     """
+#     pass
